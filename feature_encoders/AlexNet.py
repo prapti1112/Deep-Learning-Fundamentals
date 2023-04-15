30d3471a15b6ac5bb860fb2ff1479dc2f0ab2c60
prapti1112 <praptipatil01@gmail.com>
2023-04-15 13:28:41 +0530

    Initial commit

---
+++ feature_encoders/AlexNet.py
@@ -0,0 +1,5 @@
+"""
+    This file contains a tensorflow implementation of AlexNet as reffered to 
+    in the paper: https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
+    This implementation is tested on a tor dataset: <Dataset name>
+"""
